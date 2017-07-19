b0a0a6b9ddc20cc25892e4ca4e4020b1b87a404e
Kevin <kevinzbw@gmail.com>
2017-07-19 17:11:03 -0500

    turned to use MathematicaScript

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ def create_dirs_from_path(path):
 
 def main():
     parser = argparse.ArgumentParser(description='PSI Epsilon Generator')
-    parser.add_argument('-f', help='PSI file')
+    parser.add_argument('-f', required=True, help='PSI file')
     parser.add_argument('-o', nargs='?', help='Optional output file')
     argv = parser.parse_args(sys.argv[1:])
     if not os.path.exists(argv.f):
@@ -43,17 +43,20 @@ def main():
     psi_file_name = os.path.basename(psi_file)
     psi_file_dir = os.path.dirname(psi_file)
 
-    psi_eps_file_dir = psi_file_dir + '_eps'
+    psi_eps_file_dir = psi_file_dir + 'eps'
     psi_eps_file_name = ''.join(psi_file_name.split('.')[:-1]) + '_eps.psi'
     psi_eps_file = psi_eps_file_dir + '/' + psi_eps_file_name
 
+    math_file_dir = psi_file_dir + '_math'
+    math_file_name = ''.join(psi_file_name.split('.')[:-1]) + '_math.m'    
+    math_file = math_file_dir + '/' + math_file_name
 
     # print(psi_file, psi_file_name, psi_file_dir)
     # print(psi_eps_file, psi_eps_file_name, psi_eps_file_dir)
 
     psi_out = sp.run(['psi', psi_file, '--cdf', '--mathematica'], stdout=sp.PIPE)
     psi_out = parse_psi_output(psi_out.stdout.decode('utf-8'))
-    cdf = psi_out.split(':=')[1]
+    founction_pre = psi_out.split(':=')[0].strip().replace('_', '')
 
     with open(psi_file, 'r') as f:
         codes = f.read()
@@ -64,11 +67,19 @@ def main():
 
     psi_eps_out = sp.run(['psi', psi_eps_file, '--cdf', '--mathematica'], stdout=sp.PIPE)
     psi_eps_out = parse_psi_output(psi_eps_out.stdout.decode('utf-8'))
-    cdf_eps = psi_eps_out.split(':=')[1]
+    psi_eps_out = psi_eps_out.split(':=')
+    founction_eps_pre = 'Eps['.join(psi_eps_out[0].split('['))
+    psi_eps_out = founction_eps_pre + ':=' + psi_eps_out[-1]
+    founction_eps_pre = founction_eps_pre.strip().replace('_', '')
 
-    math_in = 'Maximize[{Abs[' + cdf + '-' + cdf_eps + '], (-1<eps<1 && (x==0 || x==1))}, {x, eps}]'
+    create_dirs_from_path(math_file_dir)
+    with open(math_file, 'w') as f:
+        f.write(psi_out + '\n')
+        f.write(psi_eps_out + '\n')
+        math_max = 'Print[Maximize[{Abs[' + founction_pre + '-' + founction_eps_pre + '], (-1<eps<1 && (x==0 || x==1))}, {x, eps}]]'
+        f.write(math_max + '\n')
 
-    math_out = sp.run(['wolframscript', '-code', math_in], stdout=sp.PIPE)
+    math_out = sp.run(['MathematicaScript', '-script', math_file], stdout=sp.PIPE)
     math_out = math_out.stdout.decode('utf-8').strip()
 
     if output_file:
