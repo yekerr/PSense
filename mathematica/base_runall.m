d02e7c2d69bf697d2cbcd623501fd0754205b78c
Zixin Huang <zixin@wirelessprv-10-194-104-226.near.illinois.edu>
2017-07-28 15:48:44 -0500

    added table functionality

--- mathematica/base_runall.m
+++ mathematica/base_runall.m
@@ -7,8 +7,8 @@ subs[p_, q_,cons_,params_] := FullSimplify[Abs[p-q],cons] /. params
 distancemax[p_,q_,cons_,vars_] := FindMaximum[{FullSimplify[Abs[p-q],cons],cons},vars]
 distancemax2[p_,q_,cons_,vars_] := NMaximize[{FullSimplify[Abs[p-q],cons],cons},vars]
 (*distancemax3[p_,q_,cons_] := Maximize[{Abs[p-q],cons},{eps,Element[r1,Integers]}]*)
-edistance[p_,q_,cons_]:=distance2[extract[f2[p]],extract[f2[FullSimplify[q,cons]]]]
-edistance2[p_,q_,cons_]:=distance2[extract[f2[p]],extract[f2[FullSimplify[q,cons]]]]
+edistance[p_,q_,cons_]:=distance2[extract[f[p]],extract[f[FullSimplify[q,cons]]]]
+edistance2[p_,q_,cons_]:=distance2[extract[f[p]],extract[f[q]]][[1]]
 extract[f_] := Values[f][[1]]
 edistancemax[p_, q_, cons_] := 
 distancemax[extract[f[p]], extract[f[FullSimplify[q,cons]]], cons]
@@ -21,16 +21,21 @@ islinear[p_] := ((NumberQ[gtd[p]])&&(NumberQ[ltd[p]]))
 (*islinear2[p_] := Module[
 		{islinearlist := {}},
 		Do[AppendTo[islinearlist,D[FullSimplify[p[i],eps>0],eps]];AppendTo[islinearlist,D[FullSimplify[p[i],eps<0],eps]],{i,-5,5}];
-		Print[islinearlist];
 		AllTrue[islinearlist, NumberQ]
-  ]*)
-
-runall[p_,np_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1)] := Module[
+  ]
+*)
+runall[p_,np_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1),filename_:"ch0x.csv"] := Module[
 	{vars := Prepend[Map[First,varsminmax],eps]
 	},
+	Print["mathematica start"];
+	$stream = OpenAppend[filename,BinaryFormat->True];
+	WriteString[$stream,$ScriptCommandLine[[1]]];
+	WriteString[$stream,","];
 	single := (Length[vars]==2);
-	If[single, runsingle[p,np,e,ne,epscons,varscons,vars,varsminmax],runmulti[p,np,(epscons&&varscons),vars,varsminmax]]
-	Print[""]
+	If[single, runsingle[p,np,e,ne,epscons,varscons,vars,varsminmax],runmulti[p,np,(epscons&&varscons),vars,varsminmax]];
+	(*Export[$stream,output,"CSV"];*)
+	Print["mathematica finished"];
+	Close[$stream]
 ]
 
 runsingle[p_,np_,e_,ne_,epscons_,varscons_,vars_,varsminmax_] := Module[
@@ -44,60 +49,141 @@ runsingle[p_,np_,e_,ne_,epscons_,varscons_,vars_,varsminmax_] := Module[
 
 runmulti[p_,np_,cons_,vars_,varsminmax_] := Module[
 	{},
+	WriteString[$stream,",,,,,"];
 	pks[p,np,cons,vars];
 	ptvd[p,np,cons,vars,varsminmax];
 	pkl[p,np,cons,vars,varsminmax];
 ]
 
+addquote[expr_] := (
+	WriteString[$stream,"\""];
+	WriteString[$stream,InputForm[expr]];
+	WriteString[$stream,"\""];
+)
+
+printmaxinval[findmaximum_] := (
+	If[Not[StringQ[findmaximum]],
+		(addquote[findmaximum[[1]]];
+		WriteString[$stream,","];
+		addquote[Values[findmaximum[[2]][[1]]]];
+		WriteString[$stream,","];
+		addquote[Values[findmaximum[[2]][[2]]]];),
+		WriteString[$stream,"error,,"]
+	]
+)
+
+printfindmax[distanceres_,cons_,vars_] := Module[
+	{},
+	addquote[distanceres];
+	WriteString[$stream,","];
+	addquote[Check[islinear[distanceres],"error"]];
+	WriteString[$stream,","];
+	findmaximum := Check[FindMaximum[{distanceres,cons},vars],"error"];
+	maximize := Check[Maximize[{distanceres,cons},vars],"error"];
+	If[Not[StringQ[findmaximum]]&&Not[StringQ[maximize]],
+		If[findmaximum[[1]]>maximize[[1]],
+			(printmaxinval[findmaximum];
+			WriteString[$stream,","]),
+			(printmaxinval[maximize];
+			WriteString[$stream,","])],
+		If[Not[StringQ[findmaximum]],
+			(printmaxinval[findmaximum];
+			WriteString[$stream,","]),
+			If[Not[StringQ[maximize]],
+				(printmaxinval[maximize];
+				WriteString[$stream,","]),
+				(WriteString[$stream,"error"];
+				WriteString[$stream,",,,"])
+			]
+		]
+	]
+]
+
 pedist[p_,q_,cons_,vars_] := Module[
 	{},
 	f[_ DiracDelta[point_]] := Solve[point==0,vars[[2]]];
-	f2[DiracDelta[point_]] := Solve[point==0,vars[[2]]];
-	edistanceres := edistance[p,q,cons][[1]];
-	Print["edistance"];
-	Print[edistanceres];
-	Print["edistanceMax"];
-	Print[FindMaximum[{edistanceres,cons},vars]];
-	Print[Maximize[{edistanceres,cons},vars]];
-	Print[""]
+	f[DiracDelta[point_]] := Solve[point==0,vars[[2]]];
+	edistanceres := Check[edistance[p,q,cons][[1]],"error"];
+	edistanceres2 := Check[edistance2[p,q,cons][[1]],"error"];
+	If[Not[StringQ[edistanceres]],
+		printfindmax[edistanceres,cons,vars],
+		If[Not[StringQ[edistanceres2]],
+			printfindmax[edistanceres2,cons,vars]
+			,WriteString[$stream,"error,,,,,"]
+		]
+	];
 ]
 
+
+findlarger[expr1_,expr2_] := (
+	If[Not[StringQ[expr1]]&&Not[StringQ[expr2]],
+		If[expr1[[1]]>expr2[[1]],
+			Return[expr1],
+			Return[expr2]
+		],
+		If[Not[StringQ[expr1]],
+			Return[expr1],
+			If[Not[StringQ[expr2]],
+				Return[expr2],
+				Return["error,,"]
+			]
+		]
+	]
+	
+)
+
 pks[p_,q_,cons_,vars_] := Module[
-	{disres := FullSimplify[distance[p,q,cons],cons],
-	distancemaxres := distancemax[p,q,cons,vars],
-	distancemax2res := distancemax2[p,q,cons,vars]
+	{
 	},
-	Print["distance"];
-	Print[disres];
-	Print["ksmax(distancemax)"];
-	Print[distancemaxres];
-	Print[distancemax2res];
-	Print[""]
+	disres := Check[FullSimplify[distance[p,q,cons],cons],"error"];
+	addquote[disres];
+	WriteString[$stream,","];
+	If[Not[StringQ[disres]],
+		(disresr2[r2_] = (disres /. r1->r2);
+		addquote[islinear2[disresr2]];
+		WriteString[$stream,","];
+		distancemaxres := Check[distancemax[p,q,cons,vars],"error"];
+		distancemax2res := Check[distancemax2[p,q,cons,vars],"error"];
+		printmaxinval[findlarger[distancemaxres,distancemax2res]];
+		WriteString[$stream,","]),
+		WriteString[$stream, ",,"]
+	];
 ]
 ptvd[p_,q_,cons_,vars_,varsminmax_] := Module[
-	{tvdres := tvd[p,q,cons,varsminmax]
+	{tvdres := Check[tvd[p,q,cons,varsminmax],"error"]
 	},
-	Print["tvd"];
-	Print[tvdres];
-	Print["tvdMax"];
-	Print[FindMaximum[{tvdres,cons},vars]];
-	Print[Maximize[{tvdres,cons},vars]];
-	Print[""]
+	addquote[tvdres];
+	WriteString[$stream,","];
+	If[Not[StringQ[tvdres]],(
+		addquote[islinear[tvdres]];
+		WriteString[$stream,","];
+		findmaximum := Check[FindMaximum[{tvdres,cons},vars],"error"];
+		maximize := Check[Maximize[{tvdres,cons},vars],"error"];
+		printmaxinval[findlarger[findmaximum,maximize]];
+		WriteString[$stream,","];
+		),
+		WriteString[$stream,",,"]
+	]
 ]
 pkl[p_,q_,cons_,vars_,varsminmax_] := Module[
-	{klres := kl[p,q,cons,varsminmax]
+	{klres := Check[kl[p,q,cons,varsminmax],"error"]
 	},
-	Print["kl"];
-	Print[klres];
-	Print["klMax"];
-	Print[FindMaximum[{klres,cons},vars]];
-	Print[Maximize[{klres,cons},vars]];
-	Print[""]
+	addquote[klres];
+	WriteString[$stream,","];
+	If[Not[StringQ[klres]],(
+		addquote[islinear[klres]];
+		WriteString[$stream,","];
+		findmaximum := Check[FindMaximum[{klres,cons},vars],"error"];
+		maximize := Check[Maximize[{klres,cons},vars],"error"];
+		printmaxinval[findlarger[findmaximum,maximize]];
+		WriteString[$stream,"\n"];
+		),
+		WriteString[$stream,",\n"]
+	]
 ]
 islinear2[p_] := Module[
 		{islinearlist := {}},
 		Do[AppendTo[islinearlist,DeleteDuplicates@Cases[D[FullSimplify[p[i],eps>0],eps],_Symbol,Infinity]];AppendTo[islinearlist,DeleteDuplicates@Cases[D[FullSimplify[p[i],eps<0],eps],_Symbol,Infinity]],{i,-5,5}];
-		Print[islinearlist];
 		NoneTrue[islinearlist, Function[x, MemberQ[x,Symbol["eps"]]]]
   ]
 		
