edfff57184029a4820b41afd97f3113c086f7fb7
Kevin <kevinzbw@gmail.com>
2017-12-24 13:23:52 -0600

    added build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 if [ "$(uname)" == "Darwin" ]; then
-    echo 'export PATH=/Applications/Mathematica.app/Contents/MacOS:$PATH' >> ~/.bash_profile      
+    echo 'export PATH="/Applications/Mathematica.app/Contents/MacOS:$PATH"' >> ~/.bash_profile      
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo 'export PATH=/usr/local/Wolfram/Mathematica/11.1:$PATH' >> ~/.bashrc
+    echo 'export PATH="/usr/local/Wolfram/Mathematica/11.1:$PATH"' >> ~/.bash_profile
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     setx path "%path%;%ProgramFiles%\Wolfram Research\Mathematica\11.1\wolfram"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     setx path "%path%;%ProgramFiles%\Wolfram Research\Mathematica\11.1\wolfram"
+fi
+
+if [ "$(uname)" == "Darwin" ] || [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "alias psense='python3 $(PWD)/psense.py'" >> ~/.bash_profile     
 fi
