d9a099db7fd81c2ae0a975b0e9c04b5fd937afa4
yekerr <zixinh2@illinois.edu>
2017-12-24 09:28:37 -0600

    discrete new feature half done

--- mathematica/base_runall_disc.m
+++ mathematica/base_runall_disc.m
@@ -27,39 +27,41 @@ islinear[p_] := ((NumberQ[gtd[p]])&&(NumberQ[ltd[p]]))
 		AllTrue[islinearlist, NumberQ]
   ]*)
 
-inrunall[p_,np_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1),file_:"ch0x.csv"] := Module[
+inrunall[flageps_,p_,np_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1),file_:"ch0x.csv"] := Module[
 	{
-	 vars = Prepend[Map[First,varsminmax],eps]
+    vars := Map[First,varsminmax]
 	},
-	$stream = OpenAppend["ch0x.csv",BinaryFormat->True];
+    (*$stream = OpenAppend["ch0x.csv",BinaryFormat->True];
 	WriteString[$stream,$ScriptCommandLine[[1]]];
-	WriteString[$stream,","];
-	single := (Length[vars]==2);
-	If[single, runsingle[p,np,e,ne,epscons,varscons,vars,varsminmax],runmulti[p,np,(epscons&&varscons),vars,varsminmax]]
-	Close[$stream]
+	WriteString[$stream,","];*)
+	single = (Length[vars]==1);
+    If[!flageps,vars=Prepend[vars,eps]];
+	If[single, runsingle[flageps,p,np,e,ne,epscons,varscons,vars,varsminmax],runmulti[flageps,p,np,(epscons&&varscons),vars,varsminmax]]
+    (*Close[$stream]*)
 	Print[""]
 ]
 
-runsingle[p_,np_,e_,ne_,epscons_,varscons_,vars_,varsminmax_] := Module[
-	{cons := epscons&&varscons,
-	 varsnoeps = Map[First,varsminmax]
+runsingle[flageps_,p_,np_,e_,ne_,epscons_,varscons_,vars_,varsminmax_] := Module[
+	{
 	},
-	pedist[e,ne,epscons,vars];
-	pks[p,np,cons,vars];
+    If[!flageps,cons=epscons&&varscons,cons=varscons];
+    varsnoeps = Map[First,varsminmax];
+	pedist[flageps,e,ne,epscons,cons,vars];
+	pks[flageps,p,np,cons,vars];
 	(*revkseps[ksres,varsnoeps];*) 
 	(*ptvd[p,np,cons,vars,varsminmax];
 	pkl[p,np,cons,vars,varsminmax];*)
-	ptvdsingle[p,np,cons,vars,varscons];
-	pklsingle[p,np,cons,vars,varscons];
+	ptvdsingle[flageps,p,np,cons,vars,varscons];
+	pklsingle[flageps,p,np,cons,vars,varscons];
 ]
 
-runmulti[p_,np_,cons_,vars_,varsminmax_] := Module[
+runmulti[flageps_,p_,np_,cons_,vars_,varsminmax_] := Module[
 	{},
-	varsnoeps = Map[First,varsminmax]
-	pks[p,np,cons,vars];
+	varsnoeps = Map[First,varsminmax];
+	pks[flageps,p,np,cons,vars];
 	(*revkseps[ksres,varsnoeps];*)
-	ptvd[p,np,cons,vars,varsminmax];
-	pkl[p,np,cons,vars,varsminmax];
+	ptvd[flageps,p,np,cons,vars,varsminmax];
+	pkl[flageps,p,np,cons,vars,varsminmax];
 ]
 
 revkseps[disres_,varsnoeps_] := Module[
@@ -70,60 +72,70 @@ revkseps[disres_,varsnoeps_] := Module[
 	Print["(", epsmin[[1]], ", ", epsmax[[1]], ")"];
 ]
 
-pedist[p_,q_,cons_,vars_] := Module[
+pedist[flageps_,p_,q_,epscons_,cons_,vars_] := Module[
 	{},
-	f[_ DiracDelta[point_]] := Solve[point==0,vars[[2]]];
-	f[DiracDelta[point_]] := Solve[point==0,vars[[2]]];
-	edistanceres := edistance[p,q,cons][[1]];
-	Print["edistance"];
-	(*Print[edistanceres[[1]]];*)
-	Print[edistance2[p,q,cons][[1]]];
-	Print["edistanceMax"];
-	(*Print[FindMaximum[{edistanceres,cons},vars]];*)
-	Print[Maximize[{edistanceres,cons},vars]];
-    Print["Is Linear?"];
-    Print[islinear2[edistanceres]];
+    If[flageps,onlyvar:=vars[[1]],onlyvar:=vars[[2]]];
+	f[_ DiracDelta[point_]] := Solve[point==0,onlyvar];
+	f[DiracDelta[point_]] := Solve[point==0,onlyvar];
+	edistanceres := edistance[p,q,epscons][[1]];
+	Print["Expectation Distance"];
+	Print[edistanceres];
+    (*Print[edistance2[p,q,epscons][[1]]];*)
+    If[!flageps,
+	    Print["Expectation Distance Max"];
+        (*Print[cons];*)
+	    (*Print[FindMaximum[{edistanceres,cons},vars]];*)
+	    Print[Maximize[{edistanceres,cons},vars]];
+        Print["Is Linear?"];
+        Print[islinear2[edistanceres]]];
 	Print[""]
 ]
 
-pks[p_,q_,cons_,vars_] := Module[
+pks[flageps_,p_,q_,cons_,vars_] := Module[
 	{},
 	disres = FullSimplify[distance[p,q,cons],cons];
-	(*distancemaxres = distancemax[p,q,cons,vars];
-	distancemax2res = distancemax2[p,q,cons,vars];*)
-	Print["distance"];
-	Print[disres];
-	(*Print["ksmax(distancemax)"];
-	Print[distancemaxres];
-	Print[distancemax2res];*)
-    Print["Is Linear?"];
-    disresr2[r2_] = (disres /. r1->r2);
-	Print[islinear2[disresr2[r2]]];
+    If[!flageps,
+	    Print["Distance"];
+	    Print[disres];
+	    Print["KS Distance Max"];
+	    (*distancemaxres = distancemax[p,q,cons,vars];*)
+        (*Print[distancemaxres];*)
+	    distancemax2res := distancemax2[p,q,cons,vars];
+	    Print[distancemax2res];
+        Print["Is Linear?"];
+        disresr2[r2_] = (disres /. r1->r2);
+	    Print[islinear2[disresr2[r2]]],
+    Print["KS Distance"];
+        distancemax2res := distancemax2[p,q,cons,vars];
+        Print[distancemax2res]];
 	Print[""];
 	disres
 ]
-ptvdsingle[p_,q_,cons_,vars_,varscons_] := Module[
+ptvdsingle[flageps_,p_,q_,cons_,vars_,varscons_] := Module[
 	{tvdres := tvdsingle[p,q,cons,varscons]
 	},
-	Print["tvd"];
+	Print["TVD"];
 	Print[tvdres];
-	Print["tvdMax"];
-	(*Print[FindMaximum[{tvdres,cons},vars]];*)
-	Print[Maximize[{tvdres,cons},vars]];
-    Print["Is Linear?"];
-    Print[islinear2[tvdres]];
+    If[!flageps,
+	    Print["tvdMax"];
+	    (*Print[FindMaximum[{tvdres,cons},vars]];*)
+	    Print[Maximize[{tvdres,cons},vars]];
+        Print["Is Linear?"];
+        Print[islinear2[tvdres]]];
 	Print[""]
 ]
-ptvd[p_,q_,cons_,vars_,varsminmax_] := Module[
+ptvd[flageps_,p_,q_,cons_,vars_,varsminmax_] := Module[
 	{tvdres := tvd[p,q,cons,varsminmax]
 	},
+    Print[cons];
 	Print["tvd"];
 	Print[tvdres];
-	Print["tvdMax"];
-	(*Print[FindMaximum[{tvdres,cons},vars]];*)
-	Print[Maximize[{tvdres,cons},vars]];
-    Print["Is Linear?"];
-    Print[islinear2[tvdres]];
+    If[!flageps,
+	    Print["tvdMax"];
+	    (*Print[FindMaximum[{tvdres,cons},vars]];*)
+	    Print[Maximize[{tvdres,cons},vars]];
+        Print["Is Linear?"];
+        Print[islinear2[tvdres]]];
 	Print[""]
 ]
 pkl[p_,q_,cons_,vars_,varsminmax_] := Module[
--- mathematica/base_runall_support.m
+++ mathematica/base_runall_support.m
@@ -5,7 +5,7 @@ MyDiracDelta[x_] := Boole[x == 0]
 runall[p_,pdf_,np_,flageps_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1),file_:"ch0x.csv"] := Module[
 	{
 	},
-	If[flageps==False,If[Maximize[{eps,epscons},eps][[1]]==0,newepscons:=(-0.01<=eps<=0.01),newepscons:=epscons]];
+	If[!flageps,If[Maximize[{eps,epscons},eps][[1]]==0,newepscons:=(-0.01<=eps<=0.01),newepscons:=epscons]];
 	pReplace := pdf /. DiracDelta -> MyDiracDelta;
 	newvarscons := FunctionDomain[1/Boole[0 != pReplace], Map[First,varsminmax][[1]]];
 	continuous := TrueQ[newvarscons];
@@ -17,7 +17,6 @@ runall[p_,pdf_,np_,flageps_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=e
 	Print[""];
 	If[continuous, Get[Directory[]<>"/mathematica/base_runall_cont.m"], Get[Directory[]<>"/mathematica/base_runall_disc.m"]];
 	Print["Start All Metrics:"];
-    (*Print[newvarscons];*)
 	inrunall[flageps,p,np,e,ne,varsminmax,newepscons,newvarscons,file];
 	Print["Finish All Metrics"];
 	Print[""];
