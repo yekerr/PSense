3852a5986fc069a62ccbeed280a77ce355f6745f
yekerr <zixinh2@illinois.edu>
2017-12-24 15:24:45 -0600

    fix path in runall_support

--- modules/base_runall_support.m
+++ modules/base_runall_support.m
@@ -2,7 +2,7 @@
 
 (* Input *)
 MyDiracDelta[x_] := Boole[x == 0]
-runall[p_,pdf_,np_,flageps_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1),file_:"ch0x.csv"] := Module[
+runall[mathepath_,p_,pdf_,np_,flageps_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1),file_:"ch0x.csv"] := Module[
 	{
 	},
     newepscons=True;
@@ -16,7 +16,7 @@ runall[p_,pdf_,np_,flageps_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=e
 	Print["Function Type:"];
 	If[continuous, Print["Continuous"],Print["Discrete"]];
 	Print[""];
-	If[continuous, Get[Directory[]<>"/mathematica/base_runall_cont.m"], Get[Directory[]<>"/mathematica/base_runall_disc.m"]];
+	If[continuous, Get[mathepath<>"/base_runall_cont.m"], Get[mathepath<>"/base_runall_disc.m"]];
 	Print["Start All Metrics:"];
 	inrunall[flageps,p,np,e,ne,varsminmax,newepscons,newvarscons,file];
 	Print["Finish All Metrics"];
