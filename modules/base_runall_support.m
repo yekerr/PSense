1d33d62bcbdb053671107193268f3c0281c165de
yekerr <zixinh2@illinois.edu>
2017-12-24 18:46:51 -0600

    add example1 and fix discrete detection

--- modules/base_runall_support.m
+++ modules/base_runall_support.m
@@ -9,7 +9,7 @@ runall[mathepath_,p_,pdf_,np_,flageps_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons
 	If[!flageps,If[Maximize[{eps,epscons},eps][[1]]==0,newepscons=(-0.01<=eps<=0.01),newepscons=epscons]];
 	pReplace := pdf /. DiracDelta -> MyDiracDelta;
 	newvarscons := FunctionDomain[1/Boole[0 != pReplace], Map[First,varsminmax][[1]]];
-	continuous := TrueQ[newvarscons]||MatchQ[newvarscons, Inequality_];
+	continuous := TrueQ[newvarscons]||MatchQ[newvarscons,__Inequality];
 	Print[""];
 	Print["Start Mathematica"];
 	Print[""];
