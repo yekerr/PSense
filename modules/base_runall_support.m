5d4d1d01a471d248f198b7deeea14ac617ba9f96
yekerr <zixinh2@illinois.edu>
2018-01-22 22:49:59 -0600

    bug fix

--- modules/base_runall_support.m
+++ modules/base_runall_support.m
@@ -2,7 +2,7 @@
 
 (* Input *)
 MyDiracDelta[x_] := Boole[x == 0]
-runall[mathepath_,p_,pdf_,np_,flageps_,flagexpdist_,flagks_,flagtvd_,flagkl_,flagcustom_,customfun_,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1),file_:"ch0r.csv"] := Module[
+runall[mathepath_,p_,pdf_,np_,flageps_,flagexpdist_,flagks_,flagtvd_,flagkl_,flagcustom_,customfun_:0,e_:1,ne_:1,varsminmax_:{{r1,0,1}},epscons_:(-0.01<=eps<=0.01),varscons_:(r1==0||r1==1),file_:"ch0r.csv"] := Module[
 	{
 	},
 	newepscons=If[!flageps,If[Maximize[{eps,epscons},eps][[1]]==0,(-0.01<=eps<=0.01),epscons],True];
